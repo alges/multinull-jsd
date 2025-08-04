819bb889e879a38fd5cd42be8e63ec08307f0af8
Camilo Ramírez <camilo.gvx@gmail.com>
2025-08-04 12:58:33 -0400

    Minor editions on type-hinting

--- multinull_jsd/core.py
+++ multinull_jsd/core.py
@@ -1,6 +1,6 @@
 from multinull_jsd.types import CDFBackendName
 from null_structures import IndexedHypotheses
-from typing import Optional, Union, Sequence
+from typing import Optional, Sequence
 
 import numpy.typing as npt
 
@@ -13,10 +13,10 @@ class MultiNullJSDTest:
     ) -> None:
         pass
 
-    def add_nulls(self, prob_vector: npt.ArrayLike, target_alpha: Union[float, Sequence[float]]) -> None:
+    def add_nulls(self, prob_vector: npt.ArrayLike, target_alpha: float | Sequence[float]) -> None:
         pass
 
-    def remove_nulls(self, null_index: Union[int, Sequence[int]]) -> None:
+    def remove_nulls(self, null_index: int | Sequence[int]) -> None:
         pass
 
     def get_nulls(self) -> IndexedHypotheses:
@@ -28,7 +28,7 @@ class MultiNullJSDTest:
     def infer_decisions(self, query: npt.ArrayLike) -> npt.NDArray[int]:
         pass
 
-    def get_alpha(self, null_index: Union[int, Sequence[int]]) -> float:
+    def get_alpha(self, null_index: int | Sequence[int]) -> float:
         pass
 
     def get_beta(self, query: npt.ArrayLike) -> float:
